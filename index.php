0ef90472d975dedc0b772511e5cb78b563ded61a
yiwei <yiweihsutw@gmail.com>
2018-07-07 22:25:44 +0200

    init again

--- index.php
+++ index.php
@@ -1,11 +1,28 @@
 <?php
 
-include 'Template.php';
+$rules =  array(
+	'/{\/(foreach)}/i' => '<?php } ?>',
+	'/{foreach\s+from=(\S+)\s+item=(\S+)}/is' => '<?php if(is_array($1)) foreach($1 as $$2) { ?>',
+	'/{\s*(\S+)\s*}/is' =>  '<?php echo $1;?>',
+);
 
-$template = new Template;
+//-- 解析模板 start
+$html = file_get_contents('template.tmpl');
 
-$template->assign('username', 'Yiwei');
+echo $html;
 
-$template->render('template.tmpl');
+$html = preg_replace(array_keys($rules), $rules, $html);
+
+//-- 缓存解析后的模板
+file_put_contents('tpl_cache.php', $html);
+
+//-- 要注入模板的变量
+$data = ['names'=>['李白', '花木兰']];
+
+//-- 注入变量
+extract($data);
+
+//-- 引入解析的模板
+include 'tpl_cache.php';
 
 ?>
